3e8f5c6d65dd4ec27f8afffe48320277dc07d6ae
Batuhan Ata Deniz <Atadeniz02@outlook.com>
2024-10-01 04:13:34 +0200

    asda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { getTranslations } from "next-intl/server";
 import { Playfair_Display, Source_Sans_3 } from "@next/font/google";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -10,17 +11,22 @@ const playfairDisplay = Playfair_Display({
 
 const sourceSans = Source_Sans_3({
   subsets: ["latin"],
-  weight: ["400", "600", "700"], // Choose the appropriate weights you want
+  weight: ["400", "600", "700"],
   variable: "--font-source-sans-3",
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
+  params: { locale }, // Make sure to receive the locale here
 }: Readonly<{
   children: React.ReactNode;
+  params: { locale: string };
 }>) {
+  // Set the request locale
+  unstable_setRequestLocale(locale);
+
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={`${playfairDisplay.variable} ${sourceSans.variable}`}>
         {children}
       </body>
