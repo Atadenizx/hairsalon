781596a22fe1b5ef259585a8f0d8cb2f56a71c1a
Batuhan Ata Deniz <Atadeniz02@outlook.com>
2024-10-04 01:32:39 +0200

    addadsada

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -24,9 +24,7 @@ export default async function LocaleLayout({
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
-          <header>
-            <Header />
-          </header>
+          <Header />
 
           {children}
         </NextIntlClientProvider>
